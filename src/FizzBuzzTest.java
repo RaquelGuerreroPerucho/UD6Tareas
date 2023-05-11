b77f357a57083c9a00ac53558204dacc707ca16c
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-11 19:27:44 +0200

    Commit 4,5 y6

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -28,10 +28,28 @@ public class FizzBuzzTest {
     }
 
     @org.junit.jupiter.api.Test
+    void devolucionFormatoString4Test() {
+        int num=4;
+
+        String resultadoEsperado= "4";
+        String resultadoReal= FizzBuzz.getFizzBuzz(num);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @org.junit.jupiter.api.Test
     void devolucionFormatoString5Test() {
         int num=5;
 
         String resultadoEsperado= "Buzz";
+        String resultadoReal= FizzBuzz.getFizzBuzz(num);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @org.junit.jupiter.api.Test
+    void devolucionFormatoString6Test() {
+        int num=6;
+
+        String resultadoEsperado= "Fizz";
         String resultadoReal= FizzBuzz.getFizzBuzz(num);
         assertEquals(resultadoEsperado, resultadoReal);
     }
