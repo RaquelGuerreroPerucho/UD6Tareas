ecb9af4fd315721e78582219531f5a1eaa663f1d
RaquelGuerreroPerucho <raquelguerreroperucho@gmail.com>
2023-05-11 20:23:01 +0200

    Commit OperacionesTDD c y d

--- src/OperacionesTDDTest.java
+++ src/OperacionesTDDTest.java
@@ -19,6 +19,24 @@ class OperacionesTDDTest {
         assertEquals(resultadoEsperado, resultadoReal);
     }
 
+    @org.junit.jupiter.api.Test
+    void operacionesTest02() {
+        String imput="1,2";
+
+        int resultadoEsperado= 3;
+        int resultadoReal= OperacionesTDD.sumarNumeros(imput);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @org.junit.jupiter.api.Test
+    void operacionesTest03() {
+        String imput="1,1,2";
+
+        int resultadoEsperado= 4;
+        int resultadoReal= OperacionesTDD.sumarNumeros(imput);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
 
 
 }
